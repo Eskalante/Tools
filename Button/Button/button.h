bc431f508ff3f5f7fa035afa5794153233bf0e4a
Peter Sykora <peter.sykora@fel.uniza.sk>
2016-11-18 20:26:59 +0100

    Button release.

--- Button/Button/button.h
+++ Button/Button/button.h
@@ -32,12 +32,8 @@ class button {
 	// C&D
 public:
 	// provide working area
-	button(rect sArea) :bDragState(0), sButtonArea(sArea) {
-		point nill;
-		nill.x = 0;
-		nill.y = 0;
-		sStart = nill;
-		sEnd = nill;
+	button(rect sArea) :bActive(0), bFalse(0), sButtonArea(sArea) {
+
 	}
 	virtual	~button() {
 
@@ -50,69 +46,33 @@ public:
 	// OUTPUT: state of button (pushed=true)
 	bool	update(point sCursor, bool bLeftButton = false) {
 		if (bLeftButton) {
-			if (bDragState) {
-				// update
-				sEnd.x = sCursor.x;
-				sEnd.y = sCursor.y;
-			}
-			else {
-				if (isIn(sCursor))
-					// on left button down
-					sStart.x = sCursor.x;
-				sStart.y = sCursor.y;
-				sEnd.x = sCursor.x;
-				sEnd.y = sCursor.y;
-				// start dragging
-				bDragState = true;
+			if (!bActive) {
+				if (isIn(sCursor, sButtonArea) && !bFalse) {
+					// on left button down in area
+					bActive = true;
+				}
+				else
+					// false activation, button was allready pushed outside
+					bFalse = true;
 			}
 		}
 		else {
-			if (bDragState) {
+			// reset false state
+			bFalse = false;
+			if (bActive) {
+				bActive = false;
 				// on left button up
-				sEnd.x = sCursor.x;
-				sEnd.y = sCursor.y;
-				// end dragging
-				bDragState = false;
+				if(isIn(sCursor, sButtonArea))
+					return true;
 			}
 		}
-		return bDragState;
+		return false;
 	}
 
 	// MAIN
 public:
-	rect	getActiveArea() const {
-		return calcActiveArea();
-	}
-	point	getTopLeft() const {
-		point ret;
-		ret.x = calcActiveArea().left;
-		ret.y = calcActiveArea().top;
-		return ret;
-	}
-	point	getTopRight() const {
-		point ret;
-		ret.x = calcActiveArea().right;
-		ret.y = calcActiveArea().top;
-		return ret;
-	}
-	point	getBottomLeft() const {
-		point ret;
-		ret.x = calcActiveArea().left;
-		ret.y = calcActiveArea().bottom;
-		return ret;
-	}
-	point	getBottomRight() const {
-		point ret;
-		ret.x = calcActiveArea().right;
-		ret.y = calcActiveArea().bottom;
-		return ret;
-	}
-	// return vector between start and end points
-	point	getResult() const {
-		point ret;
-		ret.x = abs(sStart.x - sEnd.x);
-		ret.y = abs(sStart.y - sEnd.y);
-		return ret;
+	bool	isActive() const {
+		return bActive;
 	}
 
 	// ACC
@@ -127,50 +87,19 @@ public:
 	// ATR
 private:
 	rect	sButtonArea;	// area from which the drag starts
-	point	sStart;			// starting point
-	point	sEnd;			// ending point
-	bool	bDragState;		// status of the drag
+	bool	bActive;		// button is pushed
+	bool	bFalse;			// false activation
 
 	// HELP
 private:
 	// calculate if point is in rectangle
-	bool	isIn(point sCursor) {
-		if (sCursor.x <= sButtonArea.right &&
-			sCursor.x >= sButtonArea.left &&
-			sCursor.y <= sButtonArea.bottom &&
-			sCursor.y >= sButtonArea.top) {
+	bool	isIn(point sCursor, rect sArea) {
+		if (sCursor.x <= sArea.right &&
+			sCursor.x >= sArea.left &&
+			sCursor.y <= sArea.bottom &&
+			sCursor.y >= sArea.top) {
 			return true;
 		}
 		return false;
-	}
-	// calculate rectangle from two points (top left and bottom right)
-	rect	calcActiveArea() const {
-		rect ret;
-		// find top&bottom points
-		if (sStart.y >= sEnd.y) {
-			ret.bottom = sStart.y;
-			ret.top = sEnd.y;
-		}
-		else {
-			ret.bottom = sEnd.y;
-			ret.top = sStart.y;
-		}
-		// find left&right points
-		if (sStart.x >= sEnd.x) {
-			ret.right = sStart.x;
-			ret.left = sEnd.x;
-		}
-		else {
-			ret.right = sEnd.x;
-			ret.left = sStart.x;
-		}
-		return ret;
-	}
-	// calculate absolute value
-	int		abs(signed int value) const {
-		if (value < 0) {
-			return (-1 * value);
-		}
-		return value;
 	}
 };
--- Button/Button/main.cpp
+++ Button/Button/main.cpp
@@ -22,26 +22,35 @@ using namespace cv;
 int main() {
 	// init
 	bool exitapp = false;
+	button b(rect(HEIGHT - 100, 270, 370, HEIGHT - 50));
 	// opencv
-	namedWindow("Main", WINDOW_NORMAL);
+	namedWindow("Main", WINDOW_AUTOSIZE);
 	setMouseCallback("Main", mouse, NULL);
-	Mat screen = Mat(600, 600, CV_8UC3, Scalar(255.0, 0.0, 0.0)); // back buffer
+	Mat screen = Mat(HEIGHT, WIDTH, CV_8UC3, Scalar(255.0, 0.0, 0.0)); // back buffer
 	cur.x = 0;
 	cur.y = 0;
-	Rect area;
+	RNG rng(12345);
+	Scalar color(255.0, 0.0, 0.0);
 	// loop
 	while (!exitapp) {
 		// clear
-		screen = Scalar(255.0, 0.0, 0.0); // paint back buffer with blue color
-										  // TODO
-		if (d.update(ptp(cur), click)) { // update coordinates
-			area = rtr(d.getActiveArea()); // get current dragging area
-			rectangle(screen, area, Scalar(255.0, 255.0, 255.0, 0.5), 1); // draw this area with white rectangle 
+		screen = color; // paint back buffer with color
+		putText(screen, "Press button to change background color.", Point(10, 50), FONT_HERSHEY_SIMPLEX, 0.7, (Scalar::all(255) - color), 2); // write info with negative color
+		// TODO
+		if (b.update(ptp(cur), click)) { // update coordinates
+			// button is triggered, randomize background color
+			color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		}
-		else
-			rectangle(screen, area, Scalar(255.0, 255.0, 255.0, 0.5), -1); // after dragging leave filled white rectangle
+		if (b.isActive()) {
+			// activated, red button
+			rectangle(screen, rtr(b.getButtonArea()), Scalar(0.0, 0.0, 255.0), -1);
+		}
+		else {
+			// normal state, green button
+			rectangle(screen, rtr(b.getButtonArea()), Scalar(0.0, 255.0, 0.0), -1);
+		}
 		if (GetAsyncKeyState(VK_SPACE) & 0x8000) // press Space for reset
-			area = Rect();
+			color = Scalar(255.0, 0.0, 0.0);
 		// show
 		imshow("Main", screen);
 		// update
